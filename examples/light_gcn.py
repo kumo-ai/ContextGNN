58abe49063817c7a4fc059611690a9f502c198b6
Zecheng Zhang <zecheng@alumni.stanford.edu>
2024-09-11 16:19:04 +0000

    Add LightGCN

---
+++ examples/light_gcn.py
@@ -0,0 +1,268 @@
+"""Example script to run the models in this repository.
+
+python3 light_gcn.py --dataset rel-hm --task user-item-purchase
+"""
+
+import argparse
+import json
+import os
+from pathlib import Path
+from typing import Dict, List, Literal, Tuple
+
+import numpy as np
+import torch
+from relbench.base import Dataset, RecommendationTask, TaskType
+from relbench.datasets import get_dataset
+from relbench.modeling.graph import (
+    get_link_train_table_input,
+    make_pkey_fkey_graph,
+)
+from relbench.modeling.loader import SparseTensor
+from relbench.modeling.utils import get_stype_proposal
+from relbench.tasks import get_task
+from torch import Tensor
+from torch_frame import stype
+from torch_frame.config.text_embedder import TextEmbedderConfig
+from torch_geometric.loader import NeighborLoader
+from torch_geometric.nn.models import LightGCN
+from torch_geometric.seed import seed_everything
+from torch_geometric.typing import NodeType
+from tqdm import tqdm
+
+from hybridgnn.utils import GloveTextEmbedding
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=str, default="rel-trial")
+parser.add_argument("--task", type=str, default="site-sponsor-run")
+parser.add_argument("--lr", type=float, default=0.001)
+parser.add_argument("--epochs", type=int, default=20)
+parser.add_argument("--eval_epochs_interval", type=int, default=1)
+parser.add_argument("--batch_size", type=int, default=512)
+parser.add_argument("--channels", type=int, default=128)
+parser.add_argument("--aggr", type=str, default="sum")
+parser.add_argument("--num_layers", type=int, default=4)
+parser.add_argument("--num_neighbors", type=int, default=128)
+parser.add_argument("--temporal_strategy", type=str, default="last")
+parser.add_argument("--max_steps_per_epoch", type=int, default=2000)
+parser.add_argument("--num_workers", type=int, default=0)
+parser.add_argument("--seed", type=int, default=42)
+parser.add_argument("--cache_dir", type=str,
+                    default=os.path.expanduser("~/.cache/relbench_examples"))
+args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    torch.set_num_threads(1)
+seed_everything(args.seed)
+
+dataset: Dataset = get_dataset(args.dataset, download=True)
+task: RecommendationTask = get_task(args.dataset, args.task, download=True)
+tune_metric = "link_prediction_map"
+assert task.task_type == TaskType.LINK_PREDICTION
+
+stypes_cache_path = Path(f"{args.cache_dir}/{args.dataset}/stypes.json")
+try:
+    with open(stypes_cache_path, "r") as f:
+        col_to_stype_dict = json.load(f)
+    for table, col_to_stype in col_to_stype_dict.items():
+        for col, stype_str in col_to_stype.items():
+            col_to_stype[col] = stype(stype_str)
+except FileNotFoundError:
+    col_to_stype_dict = get_stype_proposal(dataset.get_db())
+    Path(stypes_cache_path).parent.mkdir(parents=True, exist_ok=True)
+    with open(stypes_cache_path, "w") as f:
+        json.dump(col_to_stype_dict, f, indent=2, default=str)
+
+data, col_stats_dict = make_pkey_fkey_graph(
+    dataset.get_db(),
+    col_to_stype_dict=col_to_stype_dict,
+    text_embedder_cfg=TextEmbedderConfig(
+        text_embedder=GloveTextEmbedding(device=device), batch_size=256),
+    cache_dir=f"{args.cache_dir}/{args.dataset}/materialized",
+)
+
+num_neighbors = [
+    int(args.num_neighbors // 2**i) for i in range(args.num_layers)
+]
+
+loader_dict: Dict[str, NeighborLoader] = {}
+dst_nodes_dict: Dict[str, Tuple[NodeType, Tensor]] = {}
+num_dst_nodes_dict: Dict[str, int] = {}
+for split in ["train", "val", "test"]:
+    table = task.get_table(split)
+    table_input = get_link_train_table_input(table, task)
+    dst_nodes_dict[split] = table_input.dst_nodes
+    num_dst_nodes_dict[split] = table_input.num_dst_nodes
+    loader_dict[split] = NeighborLoader(
+        data,
+        num_neighbors=num_neighbors,
+        time_attr="time",
+        input_nodes=table_input.src_nodes,
+        input_time=table_input.src_time,
+        subgraph_type="bidirectional",
+        batch_size=args.batch_size,
+        temporal_strategy=args.temporal_strategy,
+        shuffle=split == "train",
+        num_workers=args.num_workers,
+        persistent_workers=args.num_workers > 0,
+    )
+
+num_src_nodes = data[task.src_entity_table]['tf'].num_rows
+num_dst_nodes = data[task.dst_entity_table]['tf'].num_rows
+
+model = LightGCN(
+    num_src_nodes + num_dst_nodes,
+    embedding_dim=32,
+    num_layers=2,
+).to(device)
+
+
+def get_edge_index(stage: Literal["train", "val", "test"]) -> Tensor:
+    loader = loader_dict[stage]
+    sparse_tensor = SparseTensor(dst_nodes_dict[stage][1], device=device)
+    edge_index = []
+    if stage == "train":
+        total_steps = min(len(loader_dict["train"]), args.max_steps_per_epoch)
+    else:
+        total_steps = len(loader_dict[stage])
+    steps = 0
+    for batch in tqdm(loader, total=total_steps, desc=f"{stage} edge index"):
+        batch = batch.to(device)
+        input_id = batch[task.src_entity_table].input_id
+        src_batch, dst_index = sparse_tensor[input_id]
+        edge_index.append(torch.stack([src_batch, dst_index], dim=0))
+        steps += 1
+        if stage == "train" and steps > args.max_steps_per_epoch:
+            break
+    return torch.cat(edge_index, dim=1)
+
+
+def get_src_n_ids(stage: Literal["val", "test"]) -> Tensor:
+    loader = loader_dict[stage]
+    n_ids = []
+    for batch in tqdm(loader, desc=f"{stage} n_ids"):
+        batch_size = batch[task.src_entity_table].batch_size
+        # Save all entities to cpu at first
+        n_id = batch[task.src_entity_table].n_id[:batch_size].to("cpu")
+        n_ids.append(n_id)
+    return torch.cat(n_ids)
+
+
+train_edge_index = get_edge_index(stage="train").to(device)
+val_edge_index = get_edge_index(stage="val").to(device)
+# test_edge_index = get_edge_index(stage="test").to("cpu")
+# Verified the order of n_ids matches with target table eval set order
+val_n_ids = get_src_n_ids(stage="val").to(device)
+test_n_ids = get_src_n_ids(stage="test").to("cpu")
+train_loader = torch.utils.data.DataLoader(
+    range(train_edge_index.size(1)),
+    shuffle=True,
+    batch_size=args.batch_size,
+)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+
+def train() -> float:
+    model.train()
+    total_loss = total_examples = 0
+    for index in tqdm(train_loader):
+        pos_edge_label_index = train_edge_index[:, index].to(device)
+        # Destination nodes need to add num_src_nodes
+        pos_edge_label_index[1, :] += num_src_nodes
+        neg_edge_label_index = torch.stack([
+            pos_edge_label_index[0],
+            torch.randint(
+                num_src_nodes,
+                num_src_nodes + num_dst_nodes,
+                (index.numel(), ),
+                device=device,
+            )
+        ], dim=0)
+        edge_label_index = torch.cat([
+            pos_edge_label_index,
+            neg_edge_label_index,
+        ], dim=1)
+        optimizer.zero_grad()
+        pos_rank, neg_rank = model(train_edge_index, edge_label_index).chunk(2)
+        loss = model.recommendation_loss(
+            pos_rank,
+            neg_rank,
+            node_id=edge_label_index.unique(),
+        )
+        loss.backward()
+        optimizer.step()
+
+        total_loss += float(loss) * pos_rank.numel()
+        total_examples += pos_rank.numel()
+    return total_loss / total_examples
+
+
+@torch.no_grad()
+def test(
+    stage: Literal["val", "test"],
+    train_edge_index: Tensor,
+    val_edge_index: Tensor,
+    n_ids: Tensor,
+    desc: str,
+) -> np.ndarray:
+    model.eval()
+    eval_edge_index = train_edge_index
+    if stage == "test":
+        # For test set we use both train and val edges for message passing
+        eval_edge_index = torch.cat([eval_edge_index, val_edge_index], dim=1)
+    emb = model.get_embedding(eval_edge_index)
+    src_emb, dst_emb = emb[:num_src_nodes], emb[num_src_nodes:]
+    pred_list: List[Tensor] = []
+    for start in tqdm(range(0, n_ids.size(0), args.batch_size), desc=desc):
+        end = start + args.batch_size
+        n_id = n_ids[start:end]
+        logits = src_emb[n_id] @ dst_emb.t()
+        _, pred_mini = torch.topk(logits, k=task.eval_k, dim=1)
+        pred_list.append(pred_mini)
+    pred = torch.cat(pred_list, dim=0).cpu().numpy()
+    return pred
+
+
+state_dict = None
+best_val_metric = 0
+
+for epoch in range(1, args.epochs + 1):
+    train_loss = train()
+    if epoch % args.eval_epochs_interval == 0:
+        val_pred = test(
+            stage="val",
+            train_edge_index=train_edge_index,
+            val_edge_index=val_edge_index,
+            n_ids=val_n_ids,
+            desc="Val",
+        )
+        val_metrics = task.evaluate(val_pred, task.get_table("val"))
+        print(f"Epoch: {epoch:02d}, Train loss: {train_loss}, "
+              f"Val metrics: {val_metrics}")
+
+        if val_metrics[tune_metric] > best_val_metric:
+            best_val_metric = val_metrics[tune_metric]
+            state_dict = {k: v.cpu() for k, v in model.state_dict().items()}
+
+assert state_dict is not None
+model.load_state_dict(state_dict)
+val_pred = test(
+    stage="val",
+    train_edge_index=train_edge_index,
+    val_edge_index=val_edge_index,
+    n_ids=val_n_ids,
+    desc="Val",
+)
+val_metrics = task.evaluate(val_pred, task.get_table("val"))
+print(f"Best val metrics: {val_metrics}")
+
+test_pred = test(
+    stage="test",
+    train_edge_index=train_edge_index,
+    val_edge_index=val_edge_index,
+    n_ids=test_n_ids.to(device),
+    desc="Test",
+)
+test_metrics = task.evaluate(test_pred)
+print(f"Best test metrics: {test_metrics}")
