b6645b609d52b5eb8be5413227c8900c152f8aa7
Yiwen Yuan <yyuanlisette@gmail.com>
2024-09-24 04:04:33 -0700

    Update relbench_link_prediction_benchmark.py

--- benchmark/relbench_link_prediction_benchmark.py
+++ benchmark/relbench_link_prediction_benchmark.py
@@ -38,8 +38,8 @@ LINK_PREDICTION_METRIC = "link_prediction_map"
 VAL_LOSS_DELTA = 0.001
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", type=str, default="rel-amazon")
-parser.add_argument("--task", type=str, default="user-item-rate")
+parser.add_argument("--dataset", type=str, default="rel-avito")
+parser.add_argument("--task", type=str, default="user-ad-visit")
 parser.add_argument(
     "--model",
     type=str,
@@ -47,7 +47,7 @@ parser.add_argument(
     choices=["hybridgnn", "idgnn", "shallowrhsgnn"],
 )
 parser.add_argument("--epochs", type=int, default=20)
-parser.add_argument("--num_trials", type=int, default=50,
+parser.add_argument("--num_trials", type=int, default=10,
                     help="Number of Optuna-based hyper-parameter tuning.")
 parser.add_argument(
     "--num_repeats", type=int, default=5,
