d45b2de1c69c06d544d492e4a17163aa9114668c
Yiwen Yuan <yiwen@kumo.ai>
2024-10-14 08:47:00 +0000

    Add IJCAI contest dataset

--- examples/ijcai_example.py
+++ examples/ijcai_example.py
@@ -1,40 +1,19 @@
-"""Example script to run the models in this repository.
-
-python relbench_example.py --dataset rel-trial --task site-sponsor-run
-    --model hybridgnn --epochs 10
-"""
-
+import pickle
 import argparse
-import json
-import os
-import warnings
-from pathlib import Path
-from typing import Dict, List, Tuple, Union
-
-import numpy as np
+import os.path as osp
+from typing import Dict, List, Tuple
+from torch_geometric.data import HeteroData
+from torch_frame import stype
 import torch
-import torch.nn.functional as F
-from relbench.base import Dataset, RecommendationTask, TaskType
-from relbench.datasets import get_dataset
-from relbench.modeling.graph import (
-    get_link_train_table_input,
-    make_pkey_fkey_graph,
-)
-from relbench.modeling.loader import SparseTensor
-from relbench.modeling.utils import get_stype_proposal
-from relbench.tasks import get_task
+from torch_geometric.loader import NeighborLoader
+import numpy as np
+import scipy.sparse as sp
 from torch import Tensor
 from torch_frame import stype
-from torch_frame.config.text_embedder import TextEmbedderConfig
 from torch_geometric.loader import NeighborLoader
 from torch_geometric.seed import seed_everything
 from torch_geometric.typing import NodeType
-from torch_geometric.utils.cross_entropy import sparse_cross_entropy
-from tqdm import tqdm
-from hybridgnn.datasets.ijcai_contest import IJCAIContestDataset, UserItemPurchaseTask
-
-from hybridgnn.nn.models import IDGNN, HybridGNN, ShallowRHSGNN
-from hybridgnn.utils import GloveTextEmbedding, RHSEmbeddingMode
+from torch_geometric.utils import sort_edge_index
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, default="rel-ijcai")
@@ -57,42 +36,40 @@ parser.add_argument("--temporal_strategy", type=str, default="last")
 parser.add_argument("--max_steps_per_epoch", type=int, default=2000)
 parser.add_argument("--num_workers", type=int, default=0)
 parser.add_argument("--seed", type=int, default=42)
-parser.add_argument("--cache_dir", type=str,
-                    default=os.path.expanduser("~/.cache/relbench_examples"))
 args = parser.parse_args()
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if torch.cuda.is_available():
-    torch.set_num_threads(1)
 seed_everything(args.seed)
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data',
+                'ijcai-contest')
+behs = ['click', 'fav', 'cart', 'buy']
 
-dataset: Dataset = IJCAIContestDataset()
-task: RecommendationTask = UserItemPurchaseTask(dataset)
-tune_metric = "link_prediction_map"
-assert task.task_type == TaskType.LINK_PREDICTION
 
-stypes_cache_path = Path(f"{args.cache_dir}/{args.dataset}/stypes.json")
-try:
-    with open(stypes_cache_path, "r") as f:
-        col_to_stype_dict = json.load(f)
-    for table, col_to_stype in col_to_stype_dict.items():
-        for col, stype_str in col_to_stype.items():
-            col_to_stype[col] = stype(stype_str)
-except FileNotFoundError:
-    col_to_stype_dict = get_stype_proposal(dataset.get_db())
-    Path(stypes_cache_path).parent.mkdir(parents=True, exist_ok=True)
-    with open(stypes_cache_path, "w") as f:
-        json.dump(col_to_stype_dict, f, indent=2, default=str)
+data = HeteroData()
 
-db = dataset.get_db()
+def create_edge(data, behavior, beh_idx, pkey_name, pkey_idx):
+    # fkey -> pkey edges
+    edge_index = torch.stack([beh_idx, pkey_idx],dim=0)
+    edge_type = (behavior, f"f2p_{behavior}", pkey_name)
+    data[edge_type].edge_index = sort_edge_index(edge_index)
 
-data, col_stats_dict = make_pkey_fkey_graph(
-    dataset.get_db(),
-    col_to_stype_dict=col_to_stype_dict,
-    text_embedder_cfg=TextEmbedderConfig(
-        text_embedder=GloveTextEmbedding(device=device), batch_size=256),
-    cache_dir=f"{args.cache_dir}/{args.dataset}/materialized",
-)
+    # pkey -> fkey edges.
+    # "rev_" is added so that PyG loader recognizes the reverse edges
+    edge_index = torch.stack([pkey_idx, beh_idx], dim=0)
+    edge_type = (pkey_name, f"rev_f2p_{behavior}", behavior)
+    data[edge_type].edge_index = sort_edge_index(edge_index)
+
+for i in range(len(behs)):
+    behavior = behs[i]
+    with open(osp.join(path, 'trn_'+behavior), 'rb') as fs:
+        mat = pickle.load(fs)
+    if i == 0:
+        data['user'].x = torch.tensor(np.ones(mat.shape[0])).view(-1, 1)
+        data['item'].x = torch.tensor(np.ones(mat.shape[1])).view(-1, 1)
+    col_to_stype = {"time": stype.timestamp}
+    data[behavior].x = torch.tensor(mat.data)
+    coo_mat = sp.coo_matrix(mat)
+    beh_idx = torch.arange(len(coo_mat.data))
+    create_edge(data, behavior, beh_idx, 'user', torch.tensor(coo_mat.row))
+    create_edge(data, behavior, beh_idx, 'item', torch.tensor(coo_mat.col))
 
 num_neighbors = [
     int(args.num_neighbors // 2**i) for i in range(args.num_layers)
@@ -102,15 +79,14 @@ loader_dict: Dict[str, NeighborLoader] = {}
 dst_nodes_dict: Dict[str, Tuple[NodeType, Tensor]] = {}
 num_dst_nodes_dict: Dict[str, int] = {}
 for split in ["train", "val", "test"]:
-    table = task.get_table(split)
-    table_input = get_link_train_table_input(table, task)
+
     dst_nodes_dict[split] = table_input.dst_nodes
     num_dst_nodes_dict[split] = table_input.num_dst_nodes
     loader_dict[split] = NeighborLoader(
         data,
         num_neighbors=num_neighbors,
         time_attr="time",
-        input_nodes=table_input.src_nodes,
+        input_nodes='user',
         input_time=table_input.src_time,
         subgraph_type="bidirectional",
         batch_size=args.batch_size,
@@ -119,5 +95,3 @@ for split in ["train", "val", "test"]:
         num_workers=args.num_workers,
         persistent_workers=args.num_workers > 0,
     )
-    import pdb
-    pdb.set_trace()
