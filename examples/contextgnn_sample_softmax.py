5ba7bc46fbd426c07c2f4a63323a30c45d49a1c2
Yiwen Yuan <yiwen@kumo.ai>
2024-11-17 11:17:25 +0000

    change experiments

--- examples/contextgnn_sample_softmax.py
+++ examples/contextgnn_sample_softmax.py
@@ -35,16 +35,16 @@ from contextgnn.nn.models import IDGNN, ContextGNN, ShallowRHSGNN
 from contextgnn.utils import GloveTextEmbedding, RHSEmbeddingMode
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", type=str, default="rel-trial")
-parser.add_argument("--task", type=str, default="site-sponsor-run")
+parser.add_argument("--dataset", type=str, default="rel-amazon")
+parser.add_argument("--task", type=str, default="user-item-purchase")
 parser.add_argument("--lr", type=float, default=0.001)
 parser.add_argument("--epochs", type=int, default=20)
 parser.add_argument("--eval_epochs_interval", type=int, default=1)
 parser.add_argument("--batch_size", type=int, default=512)
 parser.add_argument("--channels", type=int, default=128)
 parser.add_argument("--aggr", type=str, default="sum")
-parser.add_argument("--num_layers", type=int, default=4)
-parser.add_argument("--num_neighbors", type=int, default=128)
+parser.add_argument("--num_layers", type=int, default=6)
+parser.add_argument("--num_neighbors", type=int, default=64)
 parser.add_argument("--temporal_strategy", type=str, default="last")
 parser.add_argument("--max_steps_per_epoch", type=int, default=2000)
 parser.add_argument("--num_workers", type=int, default=0)
@@ -121,7 +121,7 @@ model = ContextGNN(
     torch_frame_model_kwargs={
         "channels": 128,
         "num_layers": 4,
-    }, rhs_sample_size=100).to(device)
+    }, rhs_sample_size=1000).to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
